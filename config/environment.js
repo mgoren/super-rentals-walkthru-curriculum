79b568b8526e97d32bb6a31d7583f2d1d07c621d
Mike Goren <mgoren@mortalwombat.net>
2016-03-29 22:15:09 -0700

    after ember data and firebase lesson

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'ok-rentals',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://super-rentals-mike.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
